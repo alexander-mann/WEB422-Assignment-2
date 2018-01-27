9330c4abf3dbe81f74f0fb357d2e9b008c8368e4
alexander-mann <amann9@myseneca.ca>
2018-01-27 15:54:39 -0500

    3

--- js/main.js
+++ js/main.js
@@ -1,54 +1,83 @@
 /*********************************************************************************
- * WEB422 – Assignment 1
+ * WEB422 – Assignment 2
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  * 
- * Name: Alexander Mann | Student ID: 131-632-168 | Date: January 24, 2018
+ * Name: Alexander Mann | Student ID: 131-632-168 | Date: January 28, 2018
  * 
  *********************************************************************************/
 
-// ROM ready handler
-$(function() {
-    //$( "#data" ).append( "<h4>jQuery working!</h4>" ); // test //
-    // populate "well" (id:data) with team data
-    $( "#teams-menu" ).on( "click", function() {
-        event.preventDefault();
-        $( "#data" ).empty();
-        $( "#data" ).append( "<h3>Teams</h3>" );
-        // AJAX get call - to site with JSON data
-        $.get("https://web422-app.herokuapp.com/teams", function(data, status){
-            $( "#data" ).append( JSON.stringify(data) );
-        });
+let employeesModal = [];
+
+function initializeEmployeesModal() {
+    // fetch employee data
+    $.get("https://web422-app.herokuapp.com/employees", function (data, status) {
+        employeesModal = data;
+        refreshEmployeeRows(employeesModal);
+        if (status != "success") {
+            showGenericModal('Error', 'Unable to get Employees');
+        }
     });
-    // populate "well" (id:data) with employee data
-    $( "#employees-menu" ).on( "click", function() {
-        event.preventDefault();
-        $( "#data" ).empty();
-        $( "#data" ).append( "<h3>Employees</h3>" );
-        // AJAX get call - to site with JSON data
-        $.get("https://web422-app.herokuapp.com/employees", function(data, status){
-            $( "#data" ).append( JSON.stringify(data) );
-        });
+}
+
+function showGenericModal(title, message) {
+    // populate and show modal window
+    $(".modal-title").append(JSON.stringify(title));
+    $(".modal-body").append(JSON.stringify(message));
+    $("#genericModal").modal("show");
+}
+
+function refreshEmployeeRows(employees) {
+    // create lodash template
+    let employeeTemplate = _.template(
+        // loop through data
+        "<% _.forEach(employees, function(employee) {" +
+        // get the id
+        "<% <div class='row body-row' data-id='" +
+        "<%- employee._id %>" +
+        "'> %>" +
+        // get the first name
+        "<% <div class='col-xs-4 body-column'> %>" +
+        "<%- employee.FirstName %>" +
+        "<% </div> %>" +
+        // get the last name
+        "<% <div class='col-xs-4 body-column'> %>" +
+        "<%- employee.LastName %>" +
+        "<% </div> %>" +
+        // get the position
+        "<% <div class='col-xs-4 body-column'> %>" +
+        "<%- employee.Position.PositionName %>" +
+        "<% </div> %>" +
+        "<% </div> %>" + // close div #_id
+        "}); %>" // close forEach
+    )
+    // populate the data using the template
+    $("#employees-table").event.preventDefault();
+    $("#employees-table").appendChild(
+        employeeTemplate({ 'employees': employees })
+    );
+}
+
+function getFilteredEmployeesModel(filterString) {
+    return _.filter(employeesModal, {
+        'FirstName': filterString,
+        'LastName': filterString,
+        'Positon.PositionName': filterString
     });
-    // populate "well" (id:data) with project data
-    $( "#projects-menu" ).on( "click", function() {
-        event.preventDefault();
-        $( "#data" ).empty();
-        $( "#data" ).append( "<h3>Projects</h3>" );
-        // AJAX get call - to site with JSON data
-        $.get("https://web422-app.herokuapp.com/projects", function(data, status){
-            $( "#data" ).append( JSON.stringify(data) );
-        });
+}
+
+function getEmployeeModelById(id) {
+    return _.find(employeesModal, { '_id' : id });
+}
+
+// ROM ready-handler
+$(function () {
+    // populate data
+    initializeEmployeesModal();
+    // react to search-bar query
+    $( "#employee-search" ).keyup(function() {
+        // refresh area with filtered result
+        refreshEmployeeRows( getFilteredEmployeesModel( $('#employee-search').val() ) );
     });
-    // populate "well" (id:data) with position data
-    $( "#positions-menu" ).on( "click", function() {
-        event.preventDefault();
-        $( "#data" ).empty();
-        $( "#data" ).append( "<h3>Positions</h3>" );
-        // AJAX get call - to site with JSON data
-        $.get("https://web422-app.herokuapp.com/positions", function(data, status){
-            $( "#data" ).append( JSON.stringify(data) );
-        });
-    });
-});
+}); // end of DOM ready-handler
